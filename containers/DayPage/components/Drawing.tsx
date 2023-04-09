d8c815d1f762e4c91224bea80b8396fd51374b0d
Hailey330 <minkky330@gmail.com>
2023-04-09 23:14:59 +0900

    wip: drawing component

--- containers/DayPage/components/Drawing.tsx
+++ containers/DayPage/components/Drawing.tsx
@@ -1,5 +1,27 @@
+import styled from "@emotion/styled";
+import { MouseEventHandler } from "react";
+
 const Drawing = () => {
-  return <div>그림 일기 컴포넌트</div>;
+  // TODO: 클릭한 돌맹이 -> 깨진 이미지로 변경
+  return (
+    <Container>
+      <TodoStyle onClick={(event) => console.log(event)} />
+      <DoneStyle />
+    </Container>
+  );
 };
 
 export default Drawing;
+
+const Container = styled.div`
+  height: 525px;
+  border-bottom: 3px solid;
+`;
+
+const TodoStyle = styled.div`
+  width: 30px;
+  height: 30px;
+  background-color: red;
+`;
+
+const DoneStyle = styled.div``;
