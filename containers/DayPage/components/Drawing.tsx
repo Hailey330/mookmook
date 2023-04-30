259a503d1a9ce61444f6e4a0e1b8f97ce0fca4a6
Hailey330 <minkky330@gmail.com>
2023-04-30 22:40:52 +0900

    CSS 작업 진행

--- containers/DayPage/components/Drawing.tsx
+++ containers/DayPage/components/Drawing.tsx
@@ -1,8 +1,8 @@
 import styled from "@emotion/styled";
-import { MouseEventHandler } from "react";
 
 const Drawing = () => {
   // TODO: 클릭한 돌맹이 -> 깨진 이미지로 변경
+  // TODO: 이 영역 안에서는 마우스 커서 변경
   return (
     <Container>
       <TodoStyle onClick={(event) => console.log(event)} />
@@ -14,14 +14,16 @@ const Drawing = () => {
 export default Drawing;
 
 const Container = styled.div`
-  height: 525px;
+  height: 456px;
   border-bottom: 3px solid;
 `;
 
 const TodoStyle = styled.div`
   width: 30px;
   height: 30px;
-  background-color: red;
+  border: 1px solid purple;
+
+  cursor: pointer;
 `;
 
 const DoneStyle = styled.div``;
--- containers/DayPage/components/Script.tsx
+++ containers/DayPage/components/Script.tsx
@@ -1,5 +1,65 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
 const Script = () => {
-  return <div>원고지 부분</div>;
+  return (
+    <Container>
+      <TopContainer>
+        <TitleStyle isTitle={true}>
+          <TitleText>도전명</TitleText>
+          <TitleInput />
+        </TitleStyle>
+        <TitleStyle>
+          <TitleText>횟수</TitleText>
+          <CountStyle>+ 10 -</CountStyle>
+        </TitleStyle>
+      </TopContainer>
+      <ScriptContainer>일기장</ScriptContainer>
+    </Container>
+  );
 };
 
 export default Script;
+
+const Container = styled.div`
+  height: 25%;
+`;
+
+const TopContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 65px;
+  border-bottom: 3px solid;
+`;
+
+const ScriptContainer = styled.div`
+  height: 100%;
+`;
+
+const TitleStyle = styled.div<{ isTitle?: boolean }>`
+  display: flex;
+  width: 50%;
+  ${({ isTitle }) =>
+    isTitle
+      ? css`
+          border-right: 3px solid;
+        `
+      : ``}
+`;
+
+const TitleText = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30%;
+  border-right: 3px solid;
+
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+const TitleInput = styled.input`
+  border: none;
+`;
+
+const CountStyle = styled.div``;
